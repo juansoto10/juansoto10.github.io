78e43ca02f527379776fe3ebcf7408e293adf77c
juansoto10 <jpsoto121@gmail.com>
2022-10-04 20:57:18 -0500

    menu mobile and footer defined

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+const openMenu = document.querySelector('.mbtn-op');
+const closeMenu = document.querySelector('.mbtn-cl');
+const mainMenu = document.querySelector('.main-menu');
+const pageInner = document.querySelector('.page-inner');
 const contactButton = document.querySelector('.button-contact');
 const projectsButton = document.querySelector('.button-projects');
 
+openMenu.addEventListener('click', toggleMobileMenu);
+closeMenu.addEventListener('click', toggleMobileMenu);
 contactButton.addEventListener('click', redirectContact);
 projectsButton.addEventListener('click', redirectProjects);
+
+function toggleMobileMenu() {
+  mainMenu.classList.toggle('inactive');
+  pageInner.classList.toggle('inactive');
+  console.log('click btn-op');
+}
 
 function redirectContact() {
   window.location.href='https://github.com/juansoto10';
