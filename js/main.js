07d3a4c17d0c3d6309cc843ee7c22f0caecda092
juansoto10 <jpsoto121@gmail.com>
2022-10-05 04:24:44 -0500

    first animation in menu added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 const openMenu = document.querySelector('.mbtn-op');
 const closeMenu = document.querySelector('.mbtn-cl');
 const mainMenu = document.querySelector('.main-menu');
+const sectHome = document.querySelector('.sect-home');
 const footer = document.querySelector('footer');
-const pageInner = document.querySelector('.page-inner');
+const page = document.querySelector('.page');
 const contactButton = document.querySelector('.button-contact');
 const projectsButton = document.querySelector('.button-projects');
 
-openMenu.addEventListener('click', toggleMobileMenu);
-closeMenu.addEventListener('click', toggleMobileMenu);
+openMenu.addEventListener('click', openMobileMenu);
+closeMenu.addEventListener('click', openMobileMenu);
 contactButton.addEventListener('click', redirectContact);
 projectsButton.addEventListener('click', redirectProjects);
 
-function toggleMobileMenu() {
-  mainMenu.classList.toggle('inactive');
-  pageInner.classList.toggle('inactive');
-  footer.classList.toggle('inactive')
+// function toggleMobileMenu() {
+//   mainMenu.classList.toggle('inactive');
+//   pageInner.classList.toggle('inactive');
+//   footer.classList.toggle('inactive')
+//   console.log('click btn-op');
+// }
+
+function openMobileMenu() {
+  openMenu.classList.toggle('inactive');
   console.log('click btn-op');
+  page.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+
+  if (mainMenu.classList.contains('hide')) {
+    mainMenu.classList.remove('hide');
+    mainMenu.classList.toggle('switched')
+    
+  } else if (mainMenu.classList.contains('switched')) {
+    mainMenu.classList.remove('switched');
+    mainMenu.classList.add('hide');
+  }
+}
+
+if (mainMenu.classList.contains('switched')) {
+  sectHome.classList.toggle('inactive');
 }
 
 function redirectContact() {
