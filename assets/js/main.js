7e078c867b72f511cac6412aa004624c431e1ea6
Juan Soto <jpsoto121@gmail.com>
2023-02-28 00:24:22 -0500

    random fun facts and batabit landing page layout project added

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,11 +78,11 @@ function openMobileMenu() {
 
 function displayMenu() {
   console.log('resize or load')
-  if (window.innerWidth >= 1024) {
+  if (window.innerWidth >= 1100) {
     navLinks.classList.remove('hide');
     openMenu.classList.add('inactive');
     closeMenu.classList.add('inactive');
-  } else if (window.innerWidth < 1024) {
+  } else if (window.innerWidth < 1100) {
     navLinks.classList.add('hide');
     openMenu.classList.remove('inactive');
 
@@ -101,7 +101,7 @@ function redirectProjects() {
 }
 
 
-
+/*
 const projects = {
   countriesInfo: {
     name: 'Countries Info Website',
@@ -133,7 +133,8 @@ const projects = {
     },
     project_url: 'https://rockpaperscissorsgame-js.netlify.app'
   }
-}
+} 
+*/
 
 Object.keys(projects).forEach(key => {
   console.log(`${key}: ${projects[key]['project_url']}`);
@@ -201,31 +202,13 @@ function renderProjects() {
 renderProjects();
 
 
-const ColombiaFunFacts = [
-  `Aracataca is the city where one of the most universally known Colombians was born: Gabriel García Márquez. In 2006 there was a referendum to decide to change the name of Aracataca, becoming Aracataca-Macondo, the city where his great work “One Hundred Years of Solitude” takes place. The referendum failed since “only” 3,600 people voted (the minimum of 7,400 votes was not reached for the referendum to be legitimate) and Aracataca is still Aracataca.`,
-  `Almost 12% of the Colombian territory is part of one of the 60 national parks that exist in the country.`,
-  `Colombia is the only country in South America that has a coastline on both the Caribbean Sea and the Pacific Ocean.`,
-  `The Sierra Nevada de Santa Marta, which is a National Park, is the highest coastal mountain range in the world.`,
-  `One of the most beautiful places in Colombia is the Caño Cristales river, also called the "River of the five colors" is in the Serranía de la Macarena and between the months of July and November its bed changes color. Although it is primarily red, at times it appears yellow, green, blue, or black.`,
-  `Colombia is the world's leading producer of emeralds. There are more than 150 mines from which emeralds of the best quality are extracted. The Colombian emerald is highly prized in the sector due to its deep brilliant green hue.`,
-  `Medellín is the second largest city in Colombia. In 1991 it was known as the "murder capital of the world". Since then the city has changed a lot and is currently one of the most important cultural and economic centers in the country.`,
-  `With an altitude of 2640 meters above sea level, Bogota is the third highest capital in the world. The Colombian city is only behind Quito (Ecuador) and Sucre (Bolivia).`,
-  `One of the species that you can see in Colombia is the Inia geoffrensis better known as "pink dolphin" or "Amazon river dolphin". You can find it in the rivers of the Orinoco basin and it is characterized by its pink color. Although it is not in danger of extinction since it is not usually captured, the number of specimens has decreased along with the ideal conditions of its habitat and it is usually protected in the six South American countries in which it is present.`,
-  `A part of Colombia is on the Pacific Ring of Fire. Countries above it are more susceptible to earthquakes and volcanic eruptions.`,
-  `Colombia is a country of celebration. To the rhythm of cumbia or salsa, Colombians have plenty of reasons to celebrate. In fact, they have 18 holidays each year! Only India has more holidays than the South American country.`,
-  `The official name of the country is Republic of Colombia. The name derives from the surname of the Genoese explorer Christopher Columbus, who arrived in America in 1492.`,
-  `From 1831 to 1886 the Colombian territory had 4 different names: Republic of Nueva Granada, Granadina Confederation, United States of Colombia and finally Republic of Colombia, name that keeps today.`,
-  `Normally when we talk about Colombia, it is normal to associate it with coffee. It is the third largest exporter of coffee in the world after Brazil and Vietnam. Colombia is responsible for 12% of total coffee production worldwide. Do you want more concrete data? Don’t worry! The country exports approximately 11 million bags of coffee each year. Undoubtedly, one of the most striking curiosities of Colombia.`,
-  `Despite having so many national parks and incredible landscape beauty, Colombians live in crowded cities. In fact, 75 per cent of Colombians live in urban areas. This is a percentage above the global average that in 2010, according to the UN, stood at 51.3%. Bogota is one of the most populated cities in Latin America.`,
-  `We all know that Spanish is spoken in Colombia. In fact, more than 99% of the population speaks Spanish. However, what is not so well known is that, according to the National Indigenous Organization of Colombia (ONIC), 70 languages are spoken in Colombia: Spanish and 69 indigenous languages.`,
-  `Colombia is extremely ethnically diverse. That’s a consequence of the migrants who came to Colombia in the last 500 years. There is a very interesting mix of people descended from the natives, Spanish settlers, people who came forcibly from Africa as slaves and immigrants from the 20th century from Europe and the Middle East.`,
-  ``,
-]
-
-
 function showFunFact() {
   const p = document.createElement('p');
-  const text = document.createTextNode('');
+
+  const funFact = colombiaFunFacts[Math.floor(Math.random() * colombiaFunFacts.length)];
+  console.log(colombiaFunFacts.length)
+  // console.log(Math.floor(Math.random() * colombiaFunFacts.length))
+  const text = document.createTextNode(funFact);
   p.append(text);
   footerTextDiv.append(p);
 }
@@ -233,6 +216,9 @@ function showFunFact() {
   window.location.href='https://';
 } 
 */
+
+
+showFunFact();
 
 /* const tags = []; */
 
